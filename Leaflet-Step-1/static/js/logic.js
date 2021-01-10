412dbc5fb8c8f2bb42d766e75a3911ef29491e58
JeniLam <jenilam639@gmail.com>
2021-01-10 00:28:41 -0600

    Adds markers for quakes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,110 +1,76 @@
-// Creating map object
+// day 1 activity 10
+// Store our API endpoint inside url (Chose all earthquakes in last 7 days)
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-var myMap = L.map("mapid").setView([39.8283, -98.5795], 5)
+// Perform a GET request to the query URL
+d3.json(url, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
 
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+// day 1 activity 8/10
 // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+function createMap(earthquakes) {
+  var lightLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/light-v10",
   accessToken: API_KEY
-}).addTo(myMap);
-
-// Use this link to get the geojson data. (Chose all earthquakes in last 7 days)
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-// make function for markers to reflect magnitude of earthquake by size and depth by color. 
-// Day 1 Activity 5
-// Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
-function markerSize(magnitude) {
-  return magnitude * 10;
-};
-
-
-// make function to determine color based on magnitude (see day 2 activity 1 NY boroughs) with if else
-function markerColor(magnitude) {
-  if (magnitude > 8) {
-    return 'red'
-  } else if (magnitude > 5) {
-    return 'orange'
-  } else if (magnitude > 2) {
-    return 'yellow'
-  } else {
-    return 'green'
-  }
-};
-// try use switch once I get code working and then can adjust the opacity func as well
-// function markerColor(magnitude) {
-//   switch (magnitude) {
-//   case (magnitude > 8):
-//     return 'red';
-//   case (magnitude > 5):
-//     return "orange";
-//   case "(magnitude > 2):
-//     return "yellow";
-//   default:
-//     return "green";
-//   }
-// }
-
-
-// make function to adjust color opacity based on depth of earthquake - third coordinate on geometry.coordinates (same syntax as above just adjust opacity instead of color)
-function opacity(depth) {
-  if (depth > 8) {
-    return .8
-  } else if (depth > 5) {
-    return .6
-  } else if (depth > 2) {
-    return .4
-  } else {
-    return .3
-  }
-};
-
-// use D3 to grab data and get markers on map (day 2 activity 2)
-d3.json(url, function (response) {
-  // print response to see how to pull information from json file
-  console.log(response);
-
-  for (var i = 0; i < response.features.length; i++) {
-    var location = response.features
-
-    if (location) {
-      L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
-    }
-  }
 });
 
-// bind popups (Day 2 activity 4)
-onEachFeature: function(feature, layer) {
-  layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-    "$" + feature.properties.MHI2016);
+var satelliteLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Light : lightLayer,
+  Satellite : satelliteLayer
+};
+
+var overlayMap = {
+  Earthquakes : earthquakes
+};
+
+var myMap = L.map("mapid", {
+  center: [39.8283, -98.5795],
+  zoom: 5,
+  layers: [lightLayer, satelliteLayer]
+})
+
+// L.control.layers(baseMaps, overlayMaps).addTo(myMap)
+L.control.layers(baseMaps,overlayMap, {
+  collapsed: false
+}).addTo(myMap)
+
 }
-}).addTo(myMap);
 
-// create legend(Day 2 activity 4)
-var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
-    var labels = [];
 
-    // Add min & max
-    var legendInfo = "<h1>Median Income</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
 
-    div.innerHTML = legendInfo;
+// https://www.w3schools.com/colors/colors_picker.asp
 
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
